501d5593d8f704afb2369eb9c59d3aeaa66d2e3d
XuejieTong <xuejietong@gmail.com>
2017-03-12 16:35:43 +0800

    Signed-off-by: XuejieTong <xuejietong@gmail.com>

--- WebTonglBin/Controllers/BaseController.cs
+++ WebTonglBin/Controllers/BaseController.cs
@@ -8,18 +8,6 @@ namespace WebTonglBin.Controllers
 {
     public class BaseController : Controller
     {
-        public BaseController()
-        {
-            string s1 = string.Empty;
-            string s2 = string.Empty;
-            if (s1.Equals(s2))
-            {
-                Console.WriteLine("==");
-            }
-            else
-            {
-                Console.WriteLine("!=");
-            }
-        }
+        
     }
 }
--- WebTonglBin/Controllers/HomeController.cs
+++ WebTonglBin/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +8,26 @@ namespace WebTonglBin.Controllers
 {
     public class HomeController : BaseController
     {
-        public ActionResult Index()
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
         {
             return View();
         }

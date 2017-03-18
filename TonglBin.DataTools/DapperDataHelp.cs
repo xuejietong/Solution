80158c527d1de771a7097489d0b24e3d6f02b520
XuejieTong <xuejietong@gmail.com>
2017-03-18 11:22:45 +0800

    Signed-off-by: XuejieTong <xuejietong@gmail.com>

--- TonglBin.DataTools/DapperDataHelp.cs
+++ TonglBin.DataTools/DapperDataHelp.cs
@@ -10,20 +10,78 @@ using System.Threading.Tasks;
 
 namespace TonglBin.DataTools
 {
-    public class DapperDataHelp<T> where T : class
+    public class DapperDataHelp<T> where T : class, new()
     {
-        private static string dbConnString = string.Empty;
+        //数据库连接字符串
+        private static string dbConnString;
+        //到数据源的已打开连接
+        private static IDbConnection connection;
+        //初始化实例，静态只读
+        private static readonly DapperDataHelp<T> instance = new DapperDataHelp<T>();
 
+        /// <summary>
+        /// 初始化静态字段
+        /// </summary>
         static DapperDataHelp()
         {
             dbConnString = ConfigurationManager.ConnectionStrings["TonglBinConn"].ToString();
+            connection = new SqlConnection(dbConnString);
         }
 
-        public static Int32 Execute(string sql, T obj)
+        /// <summary>
+        /// 单例模式，私有构造函数，防止被调用者初始化
+        /// </summary>
+        private DapperDataHelp() { }
+
+        /// <summary>
+        /// 唯一获得实例方法
+        /// </summary>
+        /// <returns>DapperDataHelp实例</returns>
+        public static DapperDataHelp<T> GetDapperInstance()
         {
-            IDbConnection connection = new SqlConnection(dbConnString);
-            Int32 result = connection.Execute(sql, obj);
-            return result;
+            return instance;
+        }
+
+        //
+        // 摘要:
+        //     Execute parameterized SQL
+        //
+        // 返回结果:
+        //     Number of rows affected
+        public Int32 Execute(string sql, T obj)
+        {
+            return connection.Execute(sql, obj);
+        }
+
+        //
+        // 摘要:
+        //     Execute parameterized SQL
+        //
+        // 返回结果:
+        //     Number of rows affected
+        public Int32 Execute(string sql, List<T> objs)
+        {
+            return connection.Execute(sql, objs);
+        }
+
+        //
+        // 摘要:
+        //     Executes a query, returning the data typed as per T
+        //
+        // 返回结果:
+        //     A sequence of data of the supplied type; if a basic type (int, string, etc) is
+        //     queried then the data from the first column in assumed, otherwise an instance
+        //     is created per row, and a direct column-name===member-name mapping is assumed
+        //     (case insensitive).
+        public List<T> Query(string sql, T obj)
+        {
+            List<T> t = new List<T>();
+            var result = connection.Query<T>(sql, obj);
+            if (result.Count() > 0)
+            {
+                t = result.ToList<T>();
+            }
+            return t;
         }
     }
 }
--- TonglBin.UnitTest/Program.cs
+++ TonglBin.UnitTest/Program.cs
@@ -12,12 +12,41 @@ namespace TonglBin.UnitTest
     {
         static void Main(string[] args)
         {
-            Users user = new Users();
-            user.UserName = "ZZB";
-            user.Email = "abcde@qq.com";
-            user.Address = "北京";
-            string sql = "Insert into Users values (@UserName, @Email, @Address)";
-            DapperDataHelp<Users>.Execute(sql, user);
+            ////Insert
+            //List<Users> users = new List<Users>();
+            //Users user1 = new Users();
+            //user1.UserName = "ZZB1";
+            //user1.Email = "abcde@qq.com";
+            //user1.Address = "北京";
+            //Users user2 = new Users();
+            //user2.UserName = "ZZB2";
+            //user2.Email = "abcde@qq.com";
+            //user2.Address = "北京";
+            //users.Add(user1);users.Add(user2);
+            //string sql = "Insert into Users values (@UserName, @Email, @Address)";
+            //DapperDataHelp<Users>.Execute(sql, user);
+
+            ////Query
+            //Users user = new Users();
+            //user.UserName = "ZZB";
+            //string sql = "select * from Users where UserName=@UserName";
+            //DapperDataHelp<Users> dapperHelp = DapperDataHelp<Users>.GetDapperInstance();
+            //List<Users> users = dapperHelp.Query(sql, user);
+
+            ////Update
+            //Users user = new Users();
+            //user.UserId = 9;
+            //user.UserName = "TongXuejie";
+            //string sql = "update Users set UserName=@UserName where UserID=@UserID";
+            //DapperDataHelp<Users> dapperHelp = DapperDataHelp<Users>.GetDapperInstance();
+            //Int32 cnt = dapperHelp.Execute(sql, user);
+
+            ////Delete
+            //Users user = new Users();
+            //user.UserId = 9;
+            //string sql = "delete Users where UserID=@UserID";
+            //DapperDataHelp<Users> dapperHelp = DapperDataHelp<Users>.GetDapperInstance();
+            //Int32 cnt = dapperHelp.Execute(sql, user);
         }
     }
 }
